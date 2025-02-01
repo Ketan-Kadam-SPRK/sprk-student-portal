3775c463eb061cb423c3a908bb42bf0ec04dc801
Ketan-Kadam-SPRK <130789267+Ketan-Kadam-SPRK@users.noreply.github.com>
2025-02-01 15:14:13 +0530

    Update certificate.actions.js

--- src/components/Certification/certificate.actions.js
+++ src/components/Certification/certificate.actions.js
@@ -25,3 +25,28 @@ export const getAllCertificates = createAsyncThunk(
     }
   }
 );
+
+
+export const getCertificate = createAsyncThunk(
+  "certificate/getAllCertificates",
+  async ({ headers }) => {
+    try {
+      // Send a GET request to fetch user details using the access token and user ID
+      const res = await axiosInstance.get(
+        `https://vxljwq9k-8888.inc1.devtunnels.ms/api/student-portal/certificates`,
+        {
+          headers,
+        }
+      );
+
+      // Extract and return the data from the response
+      const data = await res.data;
+
+      // Return the user details
+      return data;
+    } catch (err) {
+      console.log(err);
+      throw err; // Throw an error if there's an issue with the request
+    }
+  }
+);
