da3cf827993576527824be5330f40574b6c6fca7
rakeshlolam <130848128+sprk2@users.noreply.github.com>
2025-02-05 18:58:48 +0530

    add : added getPreviewCertificate api

--- src/components/Certification/certificate.actions.js
+++ src/components/Certification/certificate.actions.js
@@ -46,3 +46,24 @@ export const getCertificate = createAsyncThunk(
     }
   }
 );
+
+export const getPreviewCertificate = createAsyncThunk(
+  "certificate/getPreviewCertificate",
+  async ({ headers }) => {
+    try {
+      // Send a GET request to fetch user details using the access token and user ID
+      const res = await axiosInstance.get(`/student-portal/certificates`, {
+        headers,
+      });
+
+      // Extract and return the data from the response
+      const data = await res.data;
+
+      // Return the user details
+
+      return { data: data, status: res.status };
+    } catch (err) {
+      return { status: err.response.status, error: err.response.data.error };
+    }
+  }
+);
