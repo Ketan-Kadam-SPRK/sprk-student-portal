2b1696e0ccdafd61d5045c3c93b35019601fa0ca
Ketan-Kadam-SPRK <130789267+Ketan-Kadam-SPRK@users.noreply.github.com>
2025-02-10 19:09:21 +0530

    add: check expire token in student portal

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Login from "./components/Login/Login";
@@ -6,11 +6,19 @@ import "./style.css";
 import { useSelector } from "react-redux";
 import ForgotPass from "./components/Forgot Password/ForgotPass";
 import ResetPass from "./components/Forgot Password/ResetPass";
+import useCheckTokenExpiration from "./Hooks/useCheckTokenExpiration";
 
 function App() {
+  const { checkTokenExpiration } = useCheckTokenExpiration(); // Token expiration handling
+
   const token = useSelector((state) => state.authSlice.token);
   const isAuthenticated = token ? true : false;
 
+  useEffect(() => {
+    if (checkTokenExpiration) {
+      checkTokenExpiration();
+    }
+  }, [checkTokenExpiration, token]);
   return (
     <BrowserRouter>
       <Routes>
