2d0b6654c5cab1ed0531d99587c3c527fc007968
rakeshlolam_sprk <130848128+sprk2@users.noreply.github.com>
2026-04-27 18:51:41 +0530

    add : added get Event detail By Uid and apply to event appi.

--- src/components/event/event.action.js
+++ src/components/event/event.action.js
@@ -22,3 +22,46 @@ export const getAllEvents = createAsyncThunk(
     }
   }
 );
+
+export const getEventByUid = createAsyncThunk(
+  "events/getEventByUid",
+  async ({ headers,eventUid }) => {
+    try {
+      // Send a GET request to fetch user details using the access token and user ID
+      const res = await axiosInstance.get(`/student-portal/event/${eventUid}`, {
+        headers,
+      });
+
+      // Extract and return the data from the response
+      const data = await res.data;
+
+      // Return the user details
+
+      return { data: data, status: res.status };
+    } catch (err) {
+      return { status: err.response.status, error: err.response.data.error };
+    }
+  }
+);
+
+export const applyForEvent = createAsyncThunk(
+  "events/ApplyForEvent",
+  async ({ headers, payload }) => {
+    try {
+      // Send a POST request to the login API endpoint with user data
+      const res = await axiosInstance.post(
+        `/student-portal/event/register`,
+        payload,
+        {
+          headers: headers,
+        }
+      );
+
+      const data = await res.data;
+      
+      return handleResponse(data);
+    } catch (err) {
+      return handleError(err);
+    }
+  }
+);
